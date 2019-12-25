82cb93df5989bb3ff23c7cb9359595ea08e5aa7b
hg181 <59235288+hg181@users.noreply.github.com>
2019-12-26 02:06:25 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb                push_back
+#define all(x)            (x).begin(),(x).end()
+#define int               long long
+#define M                 1000000007
+#define MM                998244353
+#define ff                first
+#define ss                second
+#define pii               pair<int,int>
+#define msi               map<string,int>
+#define mii               map<int,int>
+#define psi               pair<string,int>
+#define pis               pair<int,string>
+#define endl              "\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+clock_t time_p=clock();
+void time()
+{
+    time_p=clock()-time_p;
+    cerr<<"Time Taken : "<<(float)(time_p)/CLOCKS_PER_SEC<<"\n";
+}
+void solve(){
+  return;}
+signed main(){
+    IOS;
+    //sieve();
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    time(); return 0;}
