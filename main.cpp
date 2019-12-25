935da82a9a788e02733e465289cc3207c0508ef8
hg181 <59235288+hg181@users.noreply.github.com>
2019-12-26 02:10:42 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,8 +20,8 @@ void time()
     time_p=clock()-time_p;
     cerr<<"Time Taken : "<<(float)(time_p)/CLOCKS_PER_SEC<<"\n";
 }
-void solve(){
-  return;}
+void solve(){ int n; cin>>n;
+ cout<<endl; return;}
 signed main(){
     IOS;
     //sieve();
